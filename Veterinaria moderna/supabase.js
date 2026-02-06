7730691d5c866a6b7af233eba5165c3f750eb54f
Nelxx16 <arielneison306@gmail.com>
2026-02-05 22:26:42 -0400

    arreglo a la pagina

--- Veterinaria moderna/supabase.js
+++ Veterinaria moderna/supabase.js
@@ -1,11 +1,24 @@
-// supabase.js
-
 const SUPABASE_URL = "https://yfvomuqdvaiefctmrjut.supabase.co";
 const SUPABASE_KEY = "sb_publishable_9XjfzC3Vv2cW-QZyqXDgZA_HQNTR4Nm";
 
-window.supabaseClient = supabaseJs.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
+
+// 🔴 CERRAR SESIÓN BIEN HECHO
+window.cerrarSesion = async function () {
+  const { error } = await window.supabaseClient.auth.signOut();
+
+  if (error) {
+    console.error(error);
+    alert("No se pudo cerrar sesión");
+    return;
+  }
+
+  // Esto borra la sesión guardada
+  window.location.replace("login.html");
+};
+
 
 
